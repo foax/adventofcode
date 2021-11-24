43d8f65f4ea08f27b81743303966595b31089132
Adam Fox <adam@foxy.id.au>
2021-11-24 20:15:36 +1030

    2020 day 1 solution

---
+++ 2020/1/day1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+numbers = []
+number_re = re.compile('^\d+$')
+
+for line in sys.stdin:
+    if re.match(number_re, line):
+        numbers.append(int(line))
+
+for i, a in enumerate(numbers):
+    for b in numbers[i + 1:]:
+        if a + b == 2020:
+            print(f'a = {a}; b = {b}; a x b = {a*b}')
+        for c in numbers [i + 2:]:
+            if a + b + c == 2020:
+                print(f'a = {a}; b = {b}; c = {c}; a x b x c = {a*b*c}')
+

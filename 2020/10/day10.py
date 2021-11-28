05774e20ae6d24fa17b65bb4e7822c1466819473
Adam Fox <adam@foxy.id.au>
2021-11-28 16:57:40 +1030

    day 10: part two. Not too bad once you run through the logic on paper

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -3,6 +3,14 @@
 import sys
 from pprint import pprint
 
+
+def inc_diff_count(diff_counts, index, amount):
+    '''Helper function for incrementing a counter in a dict.'''
+    if index not in diff_counts:
+        diff_counts[index] = 0
+    diff_counts[index] += amount
+
+
 adapters = []
 for line in sys.stdin:
     adapters.append(int(line.rstrip()))
@@ -10,17 +18,27 @@ for line in sys.stdin:
 adapters.sort()
 adapters.append(adapters[-1] + 3)
 
-last = 0
+# Used to keep track of the different ways to get to a certain
+# index in the adapters list
 diff_counts = {}
-while adapters:
-    adapter = adapters.pop(0)
-    diff = adapter - last
-    if diff not in diff_counts:
-        diff_counts[diff] = 0
-    diff_counts[diff] += 1
-    last = adapter
 
-print('Diff distributions')
-pprint(diff_counts)
-print(
-    f'Multiply 1 jolt and 3 jolt differences: {diff_counts[1] * diff_counts[3]}')
+# map adapter numbers to their list index for easy lookup
+adapter_indexes = {k: v for v, k in enumerate(adapters)}
+
+for i, adapter in enumerate(adapters):
+    for j in range(1, 4):
+        adapter_to_check = adapter - j
+
+        # Don't go back past 0
+        if adapter_to_check == 0:
+            inc_diff_count(diff_counts, i, 1)
+            break
+
+        # If the adapter number we are checking is already in our list...
+        if adapter_to_check in adapter_indexes:
+            # Add the number of ways to get to that adapter number to the
+            # number of ways to get to the adapter we are checking
+            inc_diff_count(
+                diff_counts, i, diff_counts[adapter_indexes[adapter_to_check]])
+
+print(f'Number of ways to connect adapter: {diff_counts[len(adapters) - 1]}')

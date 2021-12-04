fd2abb3a5b0a23fb18dbaeb7f4a3522cf4ab7ebb
Adam Fox <adam@foxy.id.au>
2021-12-05 09:28:54 +1030

    2021/day4: tidy up

--- 2021/4/day4.py
+++ 2021/4/day4.py
@@ -4,17 +4,17 @@ import fileinput
 from pprint import pprint
 
 
-def load_input(iterator, func=lambda x: x):
-    return [func(line) for line in iterator]
+def blank_bingo_card():
+    '''Returns a blank bingo card.'''
 
-
-def blank_bingo_card_marks():
     blank_card = {'rows': [0] * 5,
                   'cols': [0] * 5,
                   'numbers': [],
                   'marks': [],
                   'won': False}
 
+    # 'marks': [[False] * 5] * 5 gives 5 copies of the same list object
+    # rather than 5 distinct list objects.
     for row in range(0, 5):
         blank_card['marks'].append([])
         for col in range(0, 5):
@@ -22,19 +22,25 @@ def blank_bingo_card_marks():
     return blank_card
 
 
-def mark_cards(bingo_cards, numbers_dict):
-    for number, coords in numbers_dict.items():
-        if not bingo_cards[number]['won']:
-            bingo_cards[number]['rows'][coords[0]] += 1
-            bingo_cards[number]['cols'][coords[1]] += 1
-            bingo_cards[number]['marks'][coords[0]][coords[1]] = True
+def mark_cards(bingo_cards, card_coords):
+    '''Mark all the bingo cards listed in card_coords'''
+
+    for card_number, coords in card_coords.items():
+        if not bingo_cards[card_number]['won']:
+            bingo_cards[card_number]['rows'][coords[0]] += 1
+            bingo_cards[card_number]['cols'][coords[1]] += 1
+            bingo_cards[card_number]['marks'][coords[0]][coords[1]] = True
 
 
 def check_card(bingo_card):
+    '''Returns true if a card has either a full row or column marked.'''
+
     return (max(bingo_card['rows'] + bingo_card['cols']) == 5)
 
 
-def count_unmarked_numbers(bingo_card):
+def sum_unmarked_numbers(bingo_card):
+    '''Returns the sum of all unmarked numbers on a bingo card.'''
+
     count = 0
     for row in range(0, 5):
         for col in range(0, 5):
@@ -45,21 +51,15 @@ def count_unmarked_numbers(bingo_card):
 
 def main():
 
+    # List of bingo numbers given by first line of input
     bingo_numbers = None
+    # List of bingo cards, populated when reading input
     bingo_cards = []
-    already_won = []
-    bingo_line_count = 0
-
-    # {
-    #   bingo_card_number:
-    #     {
-    #        board_num: (x, y),
-    #        ...
-    #     },
-    #   ...
-    # }
-
+    # A dict to track which cards have a given bingo number for quick lookup.
+    # bingo_card_numbers[bingo_number][card_num] = (x, y)
     bingo_card_numbers = {}
+
+    bingo_line_count = 0
 
     for line in fileinput.input():
         if not bingo_numbers:
@@ -68,49 +68,44 @@ def main():
             continue
         else:
             if bingo_line_count == 0:
-                bingo_card = blank_bingo_card_marks()
-                bingo_cards.append(bingo_card)
-                board_count = len(bingo_cards) - 1
+                bingo_cards.append(blank_bingo_card())
+                card_count = len(bingo_cards) - 1
+
             bingo_line = [int(x) for x in line.rstrip().split(' ') if x != '']
 
-            for row, number in enumerate(bingo_line):
+            for col, number in enumerate(bingo_line):
                 if number not in bingo_card_numbers:
                     bingo_card_numbers[number] = {}
-                bingo_card_numbers[number][board_count] = (
-                    bingo_line_count, row)
+                bingo_card_numbers[number][card_count] = (
+                    bingo_line_count, col)
 
-            bingo_card['numbers'].append(bingo_line)
+            bingo_cards[-1]['numbers'].append(bingo_line)
             bingo_line_count = (bingo_line_count + 1) % 5
 
+    last_winning_card = None
     bingo = False
     for number in bingo_numbers:
         if number not in bingo_card_numbers:
             continue
+
         mark_cards(bingo_cards, bingo_card_numbers[number])
         for card_num in bingo_card_numbers[number].keys():
             if bingo_cards[card_num]['won']:
                 continue
             bingo = check_card(bingo_cards[card_num])
-            sum = count_unmarked_numbers(bingo_cards[card_num])
+            sum = sum_unmarked_numbers(bingo_cards[card_num])
+
             if bingo:
-                if len(already_won) == 0:
-                    print(f'BINGO! Number {number}; Card number {card_num}')
+                if not last_winning_card:
+                    print(f'BINGO! Number {number}; card number {card_num}')
                     print(
-                        f'Unmarked numbers: {sum}')
-                    print(f'Multiply: {number * sum}')
-                print(f'BINGO! Number {number}; Card number {card_num}')
-                already_won.append((card_num, number))
+                        f'Unmarked numbers: {sum} Multiply: {number * sum}')
+                last_winning_card = (card_num, number)
                 bingo_cards[card_num]['won'] = True
 
-    pprint(already_won)
-    (card_num, bingo_number) = already_won[-1]
-    sum = count_unmarked_numbers(bingo_cards[card_num])
+    sum = sum_unmarked_numbers(bingo_cards[last_winning_card[0]])
     print(
-        f'The last winning card was {card_num}; bingo number was {bingo_number}; unmarked numbers sum: {sum}; multiply: {bingo_number * sum}')
-    # pprint(bingo_numbers)
-    # pprint(bingo_cards)
-    # pprint(bingo_card_numbers)
-    # pprint(bingo_card_marks)
+        f'The last winning card was {last_winning_card[0]}; bingo number was {last_winning_card[1]}; unmarked numbers sum: {sum}; multiply: {last_winning_card[1] * sum}')
 
 
 if __name__ == '__main__':

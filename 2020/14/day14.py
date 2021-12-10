7ebcb6aedadb9c668fd243372546d1eee67378f5
Adam Fox <adam.fox@sony.com>
2021-12-10 18:45:15 +1030

    2020/day14: part 2

--- 2020/14/day14.py
+++ 2020/14/day14.py
@@ -9,12 +9,15 @@ def load_input(iterator, func=lambda x: x):
 
 
 def parse_mask(mask):
-    masks = {'on': 0, 'off': 0}
+    masks = defaultdict(int)
     for i, x in enumerate(reversed(mask)):
+        mask_value = 2 ** i
         if x == '1':
-            masks['on'] += 2 ** i
+            masks['on'] += mask_value
         elif x == '0':
-            masks['off'] += 2 ** i
+            masks['off'] += mask_value
+        elif x == 'X':
+            masks['floating'] += mask_value
     return masks
 
 
@@ -24,24 +27,49 @@ def apply_mask(value, mask):
     return value
 
 
+def apply_memory_mask(value, mask):
+    values = set()
+    value = value | mask['on']
+    values.add(value)
+
+    floating_mask = mask['floating']
+    for bit in range(36):
+        if floating_mask % 2 == 1:
+            m = 2 ** bit
+            for v in list(values):
+                values.add(v | m)
+                values.add(v & ~ m)
+        floating_mask = floating_mask >> 1
+
+    return list(values)
+
+
 def main():
     lines = load_input(fileinput.input(),
                        func=lambda x: x.strip().split(' = '))
     masks = {'on': 0, 'off': 0}
-    mem = defaultdict(int)
+    mem_part1 = defaultdict(int)
+    mem_part2 = defaultdict(int)
 
     for line in lines:
         if line[0] == 'mask':
             masks = parse_mask(line[1])
             continue
         match = re.match('mem\[(\d+)\]', line[0])
-        mem_location = match.group(1)
+        mem_location = int(match.group(1))
         mem_value = int(line[1])
-        mem[mem_location] = apply_mask(mem_value, masks)
+        mem_part1[mem_location] = apply_mask(mem_value, masks)
 
-    pprint(mem)
-    mem_sum = sum([v for _, v in mem.items()])
-    print(mem_sum)
+        mem_locations = apply_memory_mask(mem_location, masks)
+        # print(f'line: {line}; mask: {masks}; mem_locations: {mem_locations}')
+        for mem_location in mem_locations:
+            mem_part2[mem_location] = mem_value
+
+    mem_sum = sum([v for _, v in mem_part1.items()])
+    print(f'part 1: {mem_sum}')
+
+    mem_sum = sum([v for _, v in mem_part2.items()])
+    print(f'part 2: {mem_sum}')
 
 
 if __name__ == '__main__':

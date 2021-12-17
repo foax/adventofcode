8273bdb9b11090b70798bbe051eec8b10685ef5e
Adam Fox <adam@foxy.id.au>
2021-12-17 19:12:33 +1030

    2021/day15: working for part 1 and 2, but is a bit slow and needs tidy up

--- 2021/15/day15.py
+++ 2021/15/day15.py
@@ -1,5 +1,6 @@
 import fileinput
 from math import sqrt
+from pprint import pprint, pformat
 
 
 def load_input(source):
@@ -15,89 +16,55 @@ def load_input(source):
 
 
 def adjacent_points(point):
-    yield (point[0], point[1] + 1)
-    yield (point[0] + 1, point[1])
-    # yield (point[0] - 1, point[1])
-    # yield (point[0], point[1] - 1)
+    for adj in (point[0], point[1] + 1), (point[0] + 1, point[1]), (point[0] - 1, point[1]), (point[0], point[1] - 1):
+        yield adj
+
+# def valid_adj_points(point, visited, extents):
 
 
-def iter_point(point, point_set, extents):
-    '''A generator that yields valid next points to try, in the order
-    of S, E, N, W. Excludes next points that would be adjacent to existing
-    points.'''
+def next_point(path_cost, visited):
+    low_cost_paths = sorted(path_cost.items(), key=lambda x: x[1])
+    for point in low_cost_paths:
+        if point[0] not in visited:
+            return point[0]
+    return False
 
-    our_set = point_set.copy()
-    # print(
-    #     f'iter point - point: {point}; point_set: {point_set}; extents: {extents}')
 
-    for next_point in adjacent_points(point):
-        # print(f'Trying adjacent point {next_point}')
-        if next_point[0] < 0 or next_point[0] > extents or next_point[1] < 0 or next_point[1] > extents:
-            # print(f'Out of bounds')
-            continue
-        if point_set & {next_point}:
-            continue
-        if (set(adjacent_points(next_point)) - {point}) & our_set:
-            # print(last_point)
-            # print(set(adjacent_points(next_point)))
-            # print((set(adjacent_points(next_point)) - {point}))
-            # print(our_set)
-            # print((set(adjacent_points(next_point)) - {point}) & our_set)
-            continue
-        yield next_point
+def lookup_risk(point, risk_values, extents):
+    lookup_point = (point[0] % extents, point[1] % extents)
+    return (risk_values[lookup_point] + (point[0] // extents + point[1] // extents) - 1) % 9 + 1
 
 
 def main():
     risk_values = load_input(fileinput.input())
-    print(risk_values)
-    extents = int(sqrt(len(risk_values))) - 1
-    coord_set = set()
+    # print(risk_values)
+    input_size = int(sqrt(len(risk_values)))
+    extent = (input_size * 5) - 1
+    visited = set()
+    path_cost = {(0, 0): 0}
 
-    # Keep a queue of [ (x, y), iteration_number ]
-    queue = [((0, 0), iter_point((0, 0), coord_set, extents))]
+    while True:
+        point = next_point(path_cost, visited)
+        # print(
+        #     f'point: {point}; len(visited): {len(visited)}; path_cost: {pformat(path_cost)}')
+        if not point:
+            break
+        visited.add(point)
+        for adj in adjacent_points(point):
+            if adj[0] < 0 or adj[0] > extent or adj[1] < 0 or adj[1] > extent:
+                continue
+            if adj in visited:
+                continue
+            # if {adjacent_points(adj)} & visited:
+            #     continue
+            cost = path_cost[point] + lookup_risk(adj, risk_values, input_size)
+            if adj in path_cost and cost >= path_cost[adj]:
+                continue
+            path_cost[adj] = cost
+        if point != (extent, extent):
+            del(path_cost[point])
 
-    # And a set of (x, y) co-ordinates for easy checking
-    coord_set = {(0, 0)}
-
-    # Keep a tab of the lowest risk found so far, no point continuing
-    # a path if the current path has more risk than this.
-    lowest_risk = None
-
-    risk = 0
-
-    iteration = -1
-    while len(queue) > 0:
-        iteration += 1
-        # print(coord_set)
-        # print([x[0] for x in queue])
-        cur_point = queue[-1]
-        if iteration % 100000 == 0:
-            print(
-                f'cur_point: {cur_point[0]}; risk: {risk}; lowest_risk: {lowest_risk}; queue length: {len(queue)}')
-
-        if cur_point[0] == (extents, extents):
-            if lowest_risk is None or risk < lowest_risk:
-                lowest_risk = risk
-        elif lowest_risk is not None and risk >= lowest_risk:
-            risk -= risk_values[(cur_point[0])]
-            coord_set.remove(cur_point[0])
-            queue.pop()
-            continue
-
-        try:
-            next_point = next(cur_point[1])
-            # print(f'next point: {next_point}')
-            queue.append((next_point, iter_point(
-                next_point, coord_set, extents)))
-            coord_set.add(next_point)
-            risk += risk_values[next_point]
-        except StopIteration:
-            # print(f'No more next points')
-            risk -= risk_values[(cur_point[0])]
-            coord_set.remove(cur_point[0])
-            queue.pop()
-
-    print(lowest_risk)
+    print(path_cost[(extent, extent)])
 
 
 if __name__ == '__main__':
